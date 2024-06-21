5fbeac8a0673f5c45668de97e02526b5600c2363
Hurrah !! <167903008+Hurrahh@users.noreply.github.com>
2024-06-21 10:11:45 +0530

    Add files via upload

---
+++ Features/Blog.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def getLLamaresponse(input_text, no_words, blog_style):
+    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash",
+                                 verbose=True,
+                                 temperature=0.5,
+                                 google_api_key=os.getenv("GOOGLE_API_KEY"))
+
+    template = """
+        Write a blog for {blog_style} job profile for a topic {input_text}
+        within {no_words} words.
+            """
+
+    prompt = PromptTemplate(input_variables=["blog_style", "input_text", 'no_words'],
+                            template=template)
+
+    final_prompt = prompt.format(blog_style=blog_style, input_text=input_text, no_words=no_words)
+    response = llm.invoke(final_prompt)
+    return response.content
+
+
+def show():
+    st.header("Generate Blogs 🤖")
+
+    input_text = st.text_input("Enter the Blog Topic")
+
+    col1, col2 = st.columns([5, 5])
+
+    with col1:
+        no_words = st.text_input('No of Words')
+    with col2:
+        blog_style = st.selectbox('Writing the blog for',
+                                  ('Researchers', 'Data Scientist', 'Common People'), index=0)
+
+    submit = st.button("Generate")
+
+
+    if submit:
+        st.write(getLLamaresponse(input_text, no_words, blog_style))
---
+++ Features/Chatbot.py
@@ -0,0 +1,54 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+import streamlit as st
+import os
+load_dotenv()
+
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+
+history = []
+
+def Generate(text):
+    prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", "You are Shinchan Nohara, Now Provide response to the user queries"),
+                ("user", "Question: {question}")
+            ]
+        )
+    llm = ChatGoogleGenerativeAI(model = "gemini-1.5-flash",
+                               verbose = True,
+                               temperature = 0.5,
+                               google_api_key = os.getenv("GOOGLE_API_KEY"))
+
+    output_parser = StrOutputParser()
+    chain = prompt | llm | output_parser
+
+    response = chain.invoke({'question':text})
+    return response
+
+def show():
+    st.markdown('''
+    # Conversational Bot Assistance
+    ''')
+
+    if chat_input:= st.chat_input("How can I assist you Today! "):
+        with st.chat_message(name='user'):
+            st.write(chat_input)
+        with st.spinner("Generating..."):
+            response = Generate(chat_input)
+        with st.chat_message(name='AI'):
+            st.write(response)
+        history.append({'user':chat_input,'assistant':response})
+
+
+
+
+
+
+
+
+
+
