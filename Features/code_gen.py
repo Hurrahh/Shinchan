af14e9bed8339e0da2e5a84d630a58f980b00ee0
Hurrah !! <167903008+Hurrahh@users.noreply.github.com>
2024-06-21 10:30:39 +0530

    Update code_gen.py

--- Features/code_gen.py
+++ Features/code_gen.py
@@ -2,7 +2,7 @@ import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from dotenv import load_dotenv
+
 import streamlit as st
 
 
